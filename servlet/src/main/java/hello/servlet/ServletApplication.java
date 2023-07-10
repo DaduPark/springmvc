ab7b9cb1872d1413ca393f6b7feb0a6b72156437
DaduPark <koreatkdtn@gmail.com>
2023-07-10 18:35:25 +0900

    test

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServletApplication {
+public class ServletApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
